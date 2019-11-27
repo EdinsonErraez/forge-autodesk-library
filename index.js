13ba6b1b60b6026f7ce1927d83cd63a429f9bd44
Erraez Edinson <erraez.edinson@gmail.com>
2019-11-27 10:15:06 -0500

    Fixed lib index

---
+++ index.js
@@ -0,0 +1,11 @@
+const Authentication = require('./lib/models/authentication');
+const Bim360 = require('./lib/models/bim-360');
+const DataManagement = require('./lib/models/data-management');
+const Generic = require('./lib/models/generic');
+
+module.exports = {
+  Authentication,
+  Bim360,
+  DataManagement,
+  Generic,
+};
