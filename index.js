549ce1cf728f2aacf2883130f25045fcc5af9129
Erraez Edinson <erraez.edinson@gmail.com>
2020-01-30 11:06:21 -0500

    added model derivative

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Authentication = require('./lib/models/authentication');
 const Bim360 = require('./lib/models/bim-360');
 const DataManagement = require('./lib/models/data-management');
+const ModelDerivative = require('./lib/models/model-derivative');
 const Generic = require('./lib/models/generic');
 
 module.exports = {
@@ -8,4 +9,5 @@ module.exports = {
   Bim360,
   DataManagement,
   Generic,
+  ModelDerivative,
 };
